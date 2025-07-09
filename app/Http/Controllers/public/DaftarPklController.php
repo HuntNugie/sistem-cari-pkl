fa9d285a0aa3a09e0a6c514b89141d00cb8f89f8
Nugie kurniawan <nugiebinhar@gmail.com>
2025-07-09 23:26:57 +0700

    Membuat fitur search dan filter jurusan di halaman tempat pkl

--- app/Http/Controllers/public/DaftarPklController.php
+++ app/Http/Controllers/public/DaftarPklController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers\public;
 
+use App\Models\Lamar;
+use App\Models\Jurusan;
 use App\Models\Lowongan;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Lamar;
 
 class DaftarPklController extends Controller
 {
-    public function daftarPkl(){
-        $lowongan = Lowongan::with(["perusahaan","jurusan","syarat"])->where("status","tersedia")->get();
-        return view("public.daftar-pkl",compact("lowongan"));
+    public function daftarPkl(Request $request){
+        $lowongan = Lowongan::with(["perusahaan","jurusan","syarat"])->where("status","tersedia")->filter($request->search)->filterjurusan($request->jurusan)->paginate(6)->withQueryString();
+        $jurusanList = Jurusan::all();
+        return view("public.daftar-pkl",compact(["lowongan","jurusanList"]));
     }
     public function detailPkl(Lowongan $lowongan){
         $user = Lamar::where("user_id",auth()->guard("web")->user()->id)->latest()->first();
